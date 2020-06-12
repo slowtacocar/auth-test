f98acea92c17e29cdb5d2703e1145ed1267e91e6
Bobby George <bobby@geotechgeeks.com>
2020-06-12 11:13:40 -0400

    Add interaction buttons

--- public/database.js
+++ public/database.js
@@ -1,13 +1,33 @@
 var db = firebase.firestore();
 
-db.collection("test-data").doc("data").get().then(function(doc) {
+var dataRef = db.collection("test-data").doc("data");
+
+dataRef.get().then(function(doc) {
   var data = doc.data();
   for (row in data) {
-    $("#table").append("<tr><td>" + row + "</td><td>" + data[row] + "</td></tr>");
+    $("#table").append("<tr><td>" + row + "</td><td>" + data[row] + "</td><td><button id='" + row + "' class='btn btn-danger delete'>Delete</button></td></tr>");
   }
+  $(".delete").click(function() {
+    var id = $(this).attr("id");
+    var object = {};
+    object[id] = firebase.firestore.FieldValue.delete();
+    dataRef.update(object).then(function() {
+      location.reload();
+    });
+  });
 });
 
 $("#sign-out").click(function() {
   firebase.auth().signOut();
   location.reload();
+});
+
+$("#add").click(function() {
+  var key = $("#key").val();
+  var value = $("#value").val();
+  var object = {};
+  object[key] = value;
+  dataRef.update(object).then(function() {
+    location.reload();
+  });
 });
